e3d37a05fe8d67d510717201d14d9ea85182918a
Vika Suhorukova <viktorya.sukhorukova@gmail.com>
2018-01-16 05:27:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(arrayMusicians, arrayInstruments) {
+  var newArray = new Array();
+  for (let i = 0; i < arrayMusicians.length; i++) {
+    newArray[i] = arrayMusicians[i]+' plays '+arrayInstruments[i];
+  }
+  return newArray;
+}
+
+function johnLennonFacts(arrayFacts) {
+ let n = 0;
+  while( n < arrayFacts.length){
+    arrayFacts[n] = arrayFacts[n]+'!!!';
+    n++;
+  }
+
+  return arrayFacts;
+}
+
+function iLoveTheBeatles(n) {
+  var newArray = new Array();
+  do {
+    newArray.push('I love the Beatles!');
+    n++;
+  } while (n < 15);
+  return newArray;
+}
