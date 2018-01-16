ba49479ba633de634423bf047715a3a1900e373b
Vika Suhorukova <viktorya.sukhorukova@gmail.com>
2018-01-16 05:22:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(arrayMusicians, arrayInstruments) {
+  var newArray = new Array();
+  for (let i = 0; i < arrayMusicians.length; i++) {
+    newArray[i] = arrayMusicians[i]+' plays '+arrayInstruments[i];
+  }
+  return newArray;
+}
+
+function johnLennonFacts(arrayFacts) {
+ let n = 0;
+  while( n < arrayFacts.length){
+    arrayFacts[n] = arrayFacts[n]+'!!!';
+    n++;
+  }
+
+  return arrayFacts;
+}
